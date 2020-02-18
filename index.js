069ff6be045c1b7ee6f25c9eea71f4c1d20bda9b
Nethelbert <gilblasse@gmail.com>
2020-02-18 17:14:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let num1 = 31;
 let num2 = 2;
 const multiply = num1 * num2;
-const random = Math.floor(Math.random() * 10);
+const random = Math.floor(Math.random() * 10) + 2;
 const mod = 53 % 7;
 const max = Math.max(1,20,3,5);
